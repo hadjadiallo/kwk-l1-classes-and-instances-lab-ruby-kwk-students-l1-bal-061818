c4f40c95b7891d0d81a2f98b36af2437b6f6f5a1
hadjadiallo <hadjadiallo0101@gmail.com>
2018-06-22 19:20:10 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,19 @@
 # person.rb
+
+ class Person 
+   def initialize (eye color, hair color,height, weight,)
+     @eye_color = eye_color
+     @hair_color  =  hair_color
+     @height  =  height
+     @weight  =  weight
+     
+     puts "i love my new #{hair_color}, it matches with my #{eye_color}"
+   end
+     
+  end 
+end 
+
+#adele_goldberg`alan_kay`
+
+adele_goldberg  = Person.new
+alan_kay = Person.new
